ad997c674c3ad0b8af9e1de0386208756cdd434e
Andrei Alisenok <andreialisenok@gmail.com>
2023-01-02 15:06:41 +0300

    fix: change index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return n >= 0
+    ? `${n}`.split('').reverse().join('')
+    : `${-n}`.split('').reverse().join('');
+};
